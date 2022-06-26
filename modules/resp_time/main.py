32a3f13d412984acdb6db895a92f430da52448ea
TheSinnerOne <salareini2@gmail.com>
2022-06-26 14:10:03 +0400

    Response base

--- modules/resp_time/main.py
+++ modules/resp_time/main.py
@@ -1,3 +1,26 @@
+from ast import Break, Continue
+import json
+from pickle import APPEND, EMPTY_LIST
 import requests
+temp_log = []
+
 response = requests.get("https://google.com")
 print(response.elapsed.total_seconds())
+while True:
+    try:
+        with open('log.json', 'r') as file_object:  
+            data = json.load(file_object)  
+    except:
+        with open('log.json', 'w') as file_object:  #open the file in write mode
+            json.dump(temp_log, file_object)
+        Continue
+    Break
+
+new_data = list(data)
+print(new_data)
+
+new_data.append(response.elapsed.total_seconds())
+with open('log.json', 'w') as file_object:  #open the file in write mode
+ json.dump(new_data, file_object)
+
+ print(new_data)
